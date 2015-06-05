9279af7a8c93bb21db4c9121fd3bfe5e2559dec0
nikolauska <nikolauska1@gmail.com>
2015-06-05 12:33:12 +0000

    latest version

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-'use strict';
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://localhost/test';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
 
-var config   = require('./config');
-var mongoose = require('./config/mongoose');
+  var collection = db.collection('users');
 
-var newSchema = mongoose.model('userNew');
-var oldSchema = mongoose.model('userOld');
+  console.log(collection);
+
+  function callback(err, docs){
+  	for (var i = 0; i < docs.length; i++) {
+  		var doc = docs[i]
+	        doc.account_type = 'standard';
+	        doc.name = doc.email;
+	        doc.providers = { basic: { email: doc.email, password: doc.password } };
+	        doc.sessions = [ { user_agent: '', token: doc.token, created_at: new Date() } ];
+	        doc.created_at = new Date();
+	        doc.edited_at = new Date();
+
+	        // Remove old properties
+	        delete doc.email;
+	        delete doc.password;
+	        delete doc.token;
+
+	        // Save the updated document
+	        collection.save(doc);
+
+	        console.log(doc);
+	};
+  	db.close();
+  }
+
+  collection.find({}).toArray(callback);
+});
+
+ 
