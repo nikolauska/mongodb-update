eece544f1504f583b25adba877d61390207c2e78
nikolauska <nikolauska1@gmail.com>
2015-06-05 09:07:19 +0300

    fixed old schema links

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         },
         url:     process.env.MONGODB_URL     || 'mongodb://localhost/test1',
         timeout: process.env.MONGODB_TIMEOUT || 5000
-    }
+    },
     mongo2: {
         opts: {
             safe: true
